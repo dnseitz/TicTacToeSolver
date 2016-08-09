49d0b3cde874721e0d923154284a4b44becde677
Daniel Seitz <dnseitz@gmail.com>
2016-08-08 19:42:48 -0700

    Initial Commit

---
+++ TicTacToeSolver/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TicTacToeSolver
+//
+//  Created by Daniel Seitz on 8/8/16.
+//  Copyright © 2016 Daniel Seitz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
